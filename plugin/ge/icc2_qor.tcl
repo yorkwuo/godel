09e8e936b73b113f98c527c481035abcac52c64d
york.wu <chihkang@scci80125.sc.intel.com>
2018-12-21 06:21:25 -0800

    n

--- plugin/ge/icc2_qor.tcl
+++ plugin/ge/icc2_qor.tcl
@@ -5,6 +5,22 @@ proc icc2_qor {infile} {
 # inst_count
       lappend elist [list inst_count $matched]
     }
+    if [regexp {^Design\s+\(Setup\)\s+(\S+)\s+(\S+)\s+(\S+)} $line whole wns tns nvp] {
+      if {$wns == "--"} {
+        lappend elist [list wns,setup $wns]]
+      } else {
+        lappend elist [list wns,setup [format "%.f" $wns]]
+      }
+      lappend elist [list nvp,setup $nvp]
+    }
+    if [regexp {^Design\s+\(Hold\)\s+(\S+)\s+(\S+)\s+(\S+)} $line whole wns tns nvp] {
+      if {$wns == "--"} {
+        lappend elist [list wns,hold $wns]]
+      } else {
+        lappend elist [list wns,hold [format "%.f" $wns]]
+      }
+      lappend elist [list nvp,hold $nvp]
+    }
   }
   close $fin
 
