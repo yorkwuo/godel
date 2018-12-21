933690c716ec9fbba778b4efd1ee875eccc873aa
york.wu <chihkang@sccj019847.sc.intel.com>
2018-12-21 03:54:14 -0800

    n

---
+++ plugin/ge/icc2_qor.tcl
@@ -0,0 +1,13 @@
+proc icc2_qor {infile} {
+    set fin  [open $infile r]
+  while {[gets $fin line] >= 0} {
+    if [regexp {Leaf Cell Count:\s+(\d+)} $line whole matched] {
+# inst_count
+      lappend elist [list inst_count $matched]
+    }
+  }
+  close $fin
+
+  return $elist
+
+}
