f702eb6650b060fe6b0921b783932af93bddc1c4
U-GAR\chihkang <chihkang@chihkang-MOBL2.gar.corp.intel.com>
2018-12-23 14:33:43 +0800

    n

---
+++ bin/genmeta.tcl
@@ -0,0 +1,9 @@
+#!/usr/bin/tclsh
+
+set pwd [pwd]
+set flist [exec find $pwd -name ".godel"]
+foreach f $flist {
+  set where [file dirname $f]
+  set fname [file tail [file dirname $f]]
+  puts [format "%-45s %s" "set meta($fname,where)" $where]
+}
