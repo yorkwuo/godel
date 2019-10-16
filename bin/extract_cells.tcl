f978cd93a15580af59060d5dd0345e58cdbc0189
York <yorkwuo@gmail.com>
2019-10-16 21:39:06 +0800

    n

--- bin/extract_cells.tcl
+++ bin/extract_cells.tcl
@@ -1,48 +1,20 @@
 #!/usr/bin/tclsh
-proc split_proc {fname odir} {
-  split_by_proc $fname
+
+proc split_by_cell {fname odir} {
+  set fin [open $fname r]
   file mkdir $odir
-
-  set ofile_name kkk
-  set count $curs(size)
-  for {set i 1} {$i <= $count} {incr i} {
-    foreach line $curs($i,all) {
-      if [regexp {^cell\s+\(\"(\w+)\"\) } $line whole matched] {
-        set ofile_name $matched
-        puts $ofile_name
-        break
-      }
-    }
-
-    set fout [open "$odir/$ofile_name.lib" w]
-      foreach line $curs($i,all) {
-        puts $fout $line
-      }
-    close $fout
-  }
-}
-proc split_by_proc {afile} {
-  upvar curs curs
-  array unset curs *
-  set fin [open $afile r]
   set num 0
   set process_no 0
+  set fout [open $odir/0dummy.kkk w]
   while {[gets $fin line] >= 0} {
     if {[regexp {^cell} $line]} {
+      close $fout
       incr num
-    }
-
-    if [regexp {^#: } $line] {
-    } elseif [regexp {^# \{\{\{} $line] {
-    } elseif [regexp {^# \}\}\}} $line] {
-    } elseif [regexp {^# vim} $line] {
-    } else {
-      lappend curs($num,all) $line
+      set fout [open $odir/$num.lib w]
     }
   }
+  close $fout
   close $fin
-  set curs(size) $num
-  #unset curs(0,all)
 }
 
 
@@ -56,4 +28,4 @@ if {$libfile == ""} {
   puts "% extract_cells.tcl foo.lib"
   return
 }
-split_proc $libfile $outdir
+split_by_cell $libfile $outdir
