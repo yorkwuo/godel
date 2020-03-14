69ac1e766b6d3fe400d087a14c7f08c741ef1b39
York <yorkwuo@gmail.com>
2020-03-14 17:40:57 +0800

    n

---
+++ bin/clean_meta.tcl
@@ -0,0 +1,26 @@
+#!/bin/sh
+# the next line restarts using tclsh \
+exec tclsh "$0" ${1+"$@"}
+
+
+source $env(GODEL_ROOT)/bin/godel.tcl
+
+source $env(GODEL_META_FILE)
+
+set kout [open "dir_not_exist.rpt" w]
+
+foreach i [array names meta] {
+  #puts $meta($i)
+  if [file exist $meta($i)] {
+  } else {
+    puts       $meta($i)
+    puts $kout $meta($i)
+    unset meta($i)
+  }
+}
+close $kout
+
+godel_array_save meta $env(GODEL_META_FILE)
+
+
+# vim:ft=tcl
