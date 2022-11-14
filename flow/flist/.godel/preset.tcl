c55c56343fc8b2fcf88938b5f5b5e481578b3a57
yorkwuo <yorkwuo@gmail.com>
2022-11-14 10:01:12 +0800

    n

---
+++ flow/flist/.godel/preset.tcl
@@ -0,0 +1,10 @@
+#!/usr/bin/tclsh
+source $env(GODEL_ROOT)/bin/godel.tcl
+
+lsetvar . last 1
+lsetvar . mtime 1
+lsetvar . name 1
+lsetvar . open 1
+lsetvar . sortby name
+lsetvar . title 1
+lsetvar . keywords 1
