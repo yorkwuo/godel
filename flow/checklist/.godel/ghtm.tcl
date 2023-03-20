5ecf4adf068c8c5d6b82fc836aaf31f3b8154f21
yorkwuo <yorkwuo@gmail.com>
2023-03-20 09:23:01 +0800

    n

--- flow/checklist/.godel/ghtm.tcl
+++ flow/checklist/.godel/ghtm.tcl
@@ -32,7 +32,7 @@ set cols ""
 cols_onoff "coldel      ; proc:bton_delete   ; D"
 cols_onoff "coltick     ; proc:bton_tick     ; T"
 lappend cols "edtable:class      ; Class"
-lappend cols "edtable:title      ; Title"
+lappend cols "edtable:g:pagename      ; Title"
 lappend cols "edtable:g:keywords ; Keywords"
 
 #local_table tbl -c $cols -serial -sortby "g:iname" -dataTables
