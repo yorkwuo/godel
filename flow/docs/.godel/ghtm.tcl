89c086385583f3a9a1d94de4e60d85f6be7b8b48
yorkwuo <yorkwuo@gmail.com>
2023-08-07 08:23:00 +0800

    n

--- flow/docs/.godel/ghtm.tcl
+++ flow/docs/.godel/ghtm.tcl
@@ -4,8 +4,8 @@ gnotes "
 
 
 "
-lappend cols "proc:ltbl_iname g:iname ; Name"
-lappend cols "edtable:g:pagename      ; title"
+#lappend cols "proc:ltbl_iname g:iname ; Name"
+lappend cols "edtable:g:pagename      ; g:pagename"
 lappend cols "proc:ltbl_lnfile  .godel/ghtm.tcl ghtm      ; ghtm"
 lappend cols "edtable:g:keywords      ; Keywords"
-local_table tbl -c $cols
+local_table tbl -c $cols -serial -dataTables
