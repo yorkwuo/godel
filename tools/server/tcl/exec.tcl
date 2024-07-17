32a68102cbfa6e31e89b482e789ebf8fb88aaab5
York <yorkwuo@gmail.com>
2024-07-17 13:43:19 +0800

    n

--- tools/server/tcl/exec.tcl
+++ tools/server/tcl/exec.tcl
@@ -1,11 +1,14 @@
-#if [file exist "$env(GODEL_DOWNLOAD)/gtcl.tcl"] {
-#  source  $env(GODEL_DOWNLOAD)/gtcl.tcl
-#  exec rm $env(GODEL_DOWNLOAD)/gtcl.tcl
-#} else {
-#  source local.tcl
-#}
 
-if [file exist "$env(GODEL_DOWNLOAD)/gtcl.tcl"] {
-  source  $env(GODEL_DOWNLOAD)/gtcl.tcl
-  exec rm $env(GODEL_DOWNLOAD)/gtcl.tcl
+set gtclfile $env(GODEL_DOWNLOAD)/gtcl.tcl
+
+if [file exist $gtclfile] {
+  after 500
+  source  $gtclfile
+  exec rm $gtclfile
+} else {
+  after 1000
+  if [file exist $gtclfile] {
+    source  $gtclfile
+    exec rm $gtclfile
+  }
 }
