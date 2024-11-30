93162f6c847a3e72b645dadb6f4ba92fad7b3c91
yorkwuo <chihkang@sccf01311925.zsc12.intel.com>
2024-11-30 05:04:52 -0800

    n

--- tools/server/tcl/opensvg.tcl
+++ tools/server/tcl/opensvg.tcl
@@ -3,6 +3,8 @@ source $env(GODEL_ROOT)/bin/godel.tcl
 
 if {[info exist env(GODEL_WSL)] && $env(GODEL_WSL) eq "1"} {
   catch {exec /mnt/c/Program\ Files/Inkscape/bin/inkscape.exe 1.svg &}
+} elseif [info exist env(INKSCAPE_BIN)] {
+  catch {exec $env(INKSCAPE_BIN) 1.svg &}
 } else {
   catch {exec inkscape 1.svg &}
 }
